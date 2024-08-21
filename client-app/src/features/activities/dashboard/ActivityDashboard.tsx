a409db367f28e27a01821b8464d8df77c0af6277
Vasilevski95 <djordje.vasilevski95@gmail.com>
2024-08-21 22:59:25 +0200

    Adding react-router

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,25 @@
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
 
-  const { selectedActivity, editMode } = activityStore;
+  const { loadActivities, activityRegistry } = activityStore;
+
+  useEffect(() => {
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [activityRegistry.size, loadActivities]);
+
+  console.log(activityRegistry);
+
+  console.log(activityRegistry.size);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading app..." />;
 
   return (
     <Grid>
@@ -16,10 +27,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        {selectedActivity && !editMode && <ActivityDetails />}
-        {/* && => anything to the right of this will execute as long as this "selectedActivity" is not null undefined
-        that way we ensure that this component is only attempted to be loaded as long as something exists in this activities array*/}
-        {editMode && <ActivityForm />}
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   );
